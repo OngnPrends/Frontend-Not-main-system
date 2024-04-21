b19d92b200fe2ad11295d3863d111d31ceb1e366
OngnPrends <166743697+OngnPrends@users.noreply.github.com>
2024-04-21 22:17:18 +0800

    Delete login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-const formElements = document.querySelectorAll('.panel-right input, .submit-btn');
-
-formElements.forEach((item, i) => {
-    setTimeout(() => {
-        item.style.opacity = 1;
-    }, i * 100);
-});
